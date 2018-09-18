6366d40dd401f7139f7bf5b9e262385140082e06
Shutaro Agematsu <Shutaro.agematsu@gmail.com>
2018-09-18 14:41:37 +0900

    client.pyを追加

---
+++ GUI/client.py
@@ -0,0 +1,32 @@
+import socket
+import random
+
+def Tsend():
+	buf = ""
+	num = 0
+	while num < 2:
+		buf += str(random.randint(-1, 1))
+		buf += ' '
+		buf += str(random.randint(-1, 1))
+		buf += ':'
+		buf += '0'
+		buf += ':'
+		num += 1
+	return buf
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket1:
+	socket1.connect(("127.0.0.1", 12345))
+
+	while True:
+		buff1 = socket1.recv(512)
+		socket1.send(b'end')
+		buff2 = socket1.recv(512)
+		socket1.send(b'end')
+
+		print(buff1)
+		print(buff2)
+
+		buffer = Tsend()
+		print(buffer);
+		socket1.send(buffer.encode('utf-8'))
+		socket1.recv(8);
